c9daabe11fd12ff3efe8e309cd8ab18680801029
SabrinaSusmi <sabrina.su512@gmail.com>
2021-03-02 10:48:58 +0600

    project setup

---
+++ ProjectUndefined/src/main/java/com/web/undefined/ProjectUndefinedApplication.java
@@ -0,0 +1,13 @@
+package com.web.undefined;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUndefinedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUndefinedApplication.class, args);
+	}
+
+}

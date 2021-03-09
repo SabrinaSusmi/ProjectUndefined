056e4c4a60b9947d68b1514eefadf0804fd0c842
SabrinaSusmi <sabrina.su512@gmail.com>
2021-03-09 16:02:11 +0600

    tables updated....ERD updated

--- DB.sql
+++ DB.sql
@@ -79,3 +79,134 @@ TABLESPACE pg_default;
 ALTER TABLE public."Customer"
     OWNER to postgres;
 
+
+--------------------------------Buy
+CREATE TABLE public."Buy"
+(
+    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    "Batch_no" character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(255) COLLATE pg_catalog."default",
+    init_qty numeric NOT NULL,
+    qty numeric NOT NULL,
+    unit_price numeric,
+    total_price numeric,
+    supplier_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    supplier_phone character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    supplier_address character varying(255) COLLATE pg_catalog."default",
+    buy_date date NOT NULL,
+    CONSTRAINT "Buy_pkey" PRIMARY KEY ("ID"),
+    CONSTRAINT batch_unique UNIQUE ("Batch_no")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Buy"
+    OWNER to postgres;
+
+
+
+--------------------------------------sell
+CREATE TABLE public."Sell"
+(
+    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(255) COLLATE pg_catalog."default",
+    qty numeric NOT NULL,
+    unit_price numeric,
+    total_price numeric,
+    sell_date date NOT NULL,
+    cust_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Sell_pkey" PRIMARY KEY ("ID"),
+    CONSTRAINT cust_sell_fk FOREIGN KEY (cust_id)
+        REFERENCES public."Customer" ("ID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Sell"
+    OWNER to postgres;
+
+
+
+----------------------------------------buy-sell-junction
+CREATE TABLE public."Buy_sell"
+(
+    batch_no character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    sell_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Buy_sell_pkey" PRIMARY KEY (batch_no, sell_id),
+    CONSTRAINT bsjunction_buy_fk FOREIGN KEY (batch_no)
+        REFERENCES public."Buy" ("Batch_no") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT bsjunction_sell_fk FOREIGN KEY (sell_id)
+        REFERENCES public."Sell" ("ID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Buy_sell"
+    OWNER to postgres;
+
+
+-----------------------------Loan
+CREATE TABLE public."Loan"
+(
+    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    amount numeric NOT NULL,
+    investor_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    investor_phone character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    investor_email character varying(255) COLLATE pg_catalog."default",
+    investor_address character varying(255) COLLATE pg_catalog."default",
+    issue_date date NOT NULL,
+    deadline date,
+    "Remaining" numeric,
+    CONSTRAINT "Loan_pkey" PRIMARY KEY ("ID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Loan"
+    OWNER to postgres;
+
+
+----------------------------Loan Records
+CREATE TABLE public."Loan_records"
+(
+    "Loan_id" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    given_amt numeric NOT NULL,
+    given_date date NOT NULL,
+    "ID" numeric NOT NULL,
+    CONSTRAINT "Loan_records_pkey" PRIMARY KEY ("ID"),
+    CONSTRAINT loan_rec_fk FOREIGN KEY ("Loan_id")
+        REFERENCES public."Loan" ("ID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Loan_records"
+    OWNER to postgres;
+
+
+--------------------------------------Expenses
+CREATE TABLE public."Expenses"
+(
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    exp_type character varying(20) COLLATE pg_catalog."default",
+    description character varying(255) COLLATE pg_catalog."default",
+    amount numeric NOT NULL,
+    exp_date date NOT NULL,
+    CONSTRAINT "Expenses_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Expenses"
+    OWNER to postgres;
+

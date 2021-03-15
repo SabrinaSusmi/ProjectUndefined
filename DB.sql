cceaff6d9913926580534618dc9975f3c3870958
SabrinaSusmi <sabrina.su512@gmail.com>
2021-03-15 13:55:42 +0600

    db table updated

--- DB.sql
+++ DB.sql
@@ -1,90 +1,89 @@
 -----------------------------------MEMBER
-
-CREATE TABLE public."Member"
+CREATE TABLE public.member
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    "Name" character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    "Email" character varying(20) COLLATE pg_catalog."default",
-    "Phone" character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    "Password" character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    "Address" character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    "DOB" date NOT NULL,
-    "Salary" numeric,
-    "Invest_amt" numeric,
-    "Withdraw_amt" numeric,
-    CONSTRAINT "Member_pkey" PRIMARY KEY ("ID"),
-    CONSTRAINT mem_info_unique UNIQUE ("Email", "Phone")
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(20) COLLATE pg_catalog."default",
+    phone character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    address character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    dob date NOT NULL,
+    salary numeric,
+    invest_amt numeric,
+    withdraw_amt numeric,
+    CONSTRAINT "Member_pkey" PRIMARY KEY (id),
+    CONSTRAINT mem_info_unique UNIQUE (email, phone)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Member"
+ALTER TABLE public.member
     OWNER to postgres;
 
 
 -------------------------------Investment
-CREATE TABLE public."Investment_records"
+CREATE TABLE public.investment_records
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     mem_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     given_amt numeric NOT NULL,
     given_date date NOT NULL,
-    CONSTRAINT inv_rec_pkey PRIMARY KEY ("ID"),
+    CONSTRAINT inv_rec_pkey PRIMARY KEY (id),
     CONSTRAINT inv_mem_fk FOREIGN KEY (mem_id)
-        REFERENCES public."Member" ("ID") MATCH SIMPLE
+        REFERENCES public.member (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Investment_records"
+ALTER TABLE public.investment_records
     OWNER to postgres;
 
 
 ---------------------------------Withdraw
-CREATE TABLE public."Withdraw_records"
+CREATE TABLE public.withdraw_records
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     mem_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     amt numeric NOT NULL,
     wtdr_date date NOT NULL,
-    CONSTRAINT "Withdraw_records_pkey" PRIMARY KEY ("ID"),
+    CONSTRAINT "Withdraw_records_pkey" PRIMARY KEY (id),
     CONSTRAINT wtdr_mem_fk FOREIGN KEY (mem_id)
-        REFERENCES public."Member" ("ID") MATCH SIMPLE
+        REFERENCES public.member (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Withdraw_records"
+ALTER TABLE public.withdraw_records
     OWNER to postgres;
 
 
 -------------------------------Customer
-CREATE TABLE public."Customer"
+CREATE TABLE public.customer
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    "Name" character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    "Email" character varying(20) COLLATE pg_catalog."default",
-    "Phone" character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    "Address" character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Customer_pkey" PRIMARY KEY ("ID"),
-    CONSTRAINT cust_info_unique UNIQUE ("Email", "Phone")
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(20) COLLATE pg_catalog."default",
+    phone character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    address character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Customer_pkey" PRIMARY KEY (id),
+    CONSTRAINT cust_info_unique UNIQUE (email, phone)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Customer"
+ALTER TABLE public.customer
     OWNER to postgres;
 
 
 --------------------------------Buy
-CREATE TABLE public."Buy"
+CREATE TABLE public.buy
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    "Batch_no" character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    batch_no character varying(30) COLLATE pg_catalog."default" NOT NULL,
     title character varying(255) COLLATE pg_catalog."default",
     init_qty numeric NOT NULL,
     qty numeric NOT NULL,
@@ -94,67 +93,67 @@ CREATE TABLE public."Buy"
     supplier_phone character varying(15) COLLATE pg_catalog."default" NOT NULL,
     supplier_address character varying(255) COLLATE pg_catalog."default",
     buy_date date NOT NULL,
-    CONSTRAINT "Buy_pkey" PRIMARY KEY ("ID"),
-    CONSTRAINT batch_unique UNIQUE ("Batch_no")
+    CONSTRAINT "Buy_pkey" PRIMARY KEY (id),
+    CONSTRAINT batch_unique UNIQUE (batch_no)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Buy"
+ALTER TABLE public.buy
     OWNER to postgres;
 
 
 
 --------------------------------------sell
-CREATE TABLE public."Sell"
+CREATE TABLE public.sell
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     title character varying(255) COLLATE pg_catalog."default",
     qty numeric NOT NULL,
     unit_price numeric,
     total_price numeric,
     sell_date date NOT NULL,
     cust_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "Sell_pkey" PRIMARY KEY ("ID"),
+    CONSTRAINT "Sell_pkey" PRIMARY KEY (id),
     CONSTRAINT cust_sell_fk FOREIGN KEY (cust_id)
-        REFERENCES public."Customer" ("ID") MATCH SIMPLE
+        REFERENCES public.customer (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Sell"
+ALTER TABLE public.sell
     OWNER to postgres;
 
 
 
 ----------------------------------------buy-sell-junction
-CREATE TABLE public."Buy_sell"
+CREATE TABLE public.buy_sell
 (
     batch_no character varying(30) COLLATE pg_catalog."default" NOT NULL,
     sell_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "Buy_sell_pkey" PRIMARY KEY (batch_no, sell_id),
     CONSTRAINT bsjunction_buy_fk FOREIGN KEY (batch_no)
-        REFERENCES public."Buy" ("Batch_no") MATCH SIMPLE
+        REFERENCES public.buy (batch_no) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
     CONSTRAINT bsjunction_sell_fk FOREIGN KEY (sell_id)
-        REFERENCES public."Sell" ("ID") MATCH SIMPLE
+        REFERENCES public.sell (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Buy_sell"
+ALTER TABLE public.buy_sell
     OWNER to postgres;
 
 
 -----------------------------Loan
-CREATE TABLE public."Loan"
+CREATE TABLE public.loan
 (
-    "ID" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     title character varying(255) COLLATE pg_catalog."default" NOT NULL,
     amount numeric NOT NULL,
     investor_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -163,38 +162,38 @@ CREATE TABLE public."Loan"
     investor_address character varying(255) COLLATE pg_catalog."default",
     issue_date date NOT NULL,
     deadline date,
-    "Remaining" numeric,
-    CONSTRAINT "Loan_pkey" PRIMARY KEY ("ID")
+    remaining numeric,
+    CONSTRAINT "Loan_pkey" PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Loan"
+ALTER TABLE public.loan
     OWNER to postgres;
 
 
 ----------------------------Loan Records
-CREATE TABLE public."Loan_records"
+CREATE TABLE public.loan_records
 (
-    "Loan_id" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    loan_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     given_amt numeric NOT NULL,
     given_date date NOT NULL,
-    "ID" numeric NOT NULL,
-    CONSTRAINT "Loan_records_pkey" PRIMARY KEY ("ID"),
-    CONSTRAINT loan_rec_fk FOREIGN KEY ("Loan_id")
-        REFERENCES public."Loan" ("ID") MATCH SIMPLE
+    id numeric NOT NULL,
+    CONSTRAINT "Loan_records_pkey" PRIMARY KEY (id),
+    CONSTRAINT loan_rec_fk FOREIGN KEY (loan_id)
+        REFERENCES public.loan (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Loan_records"
+ALTER TABLE public.loan_records
     OWNER to postgres;
 
 
 --------------------------------------Expenses
-CREATE TABLE public."Expenses"
+CREATE TABLE public.expenses
 (
     id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     title character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -207,6 +206,6 @@ CREATE TABLE public."Expenses"
 
 TABLESPACE pg_default;
 
-ALTER TABLE public."Expenses"
+ALTER TABLE public.expenses
     OWNER to postgres;
 
